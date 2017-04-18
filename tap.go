01f7c43b4c592cc6779f9cbe36a07854c50b278d
Adam Fraser <adamf@couchbase.com>
2017-04-17 20:27:15 -0700

    Adds datatype to tap event
    Required to identify feed entries containing xattr data.

--- tap.go
+++ tap.go
@@ -24,6 +24,7 @@ type TapEvent struct {
 	Key, Value []byte    // Item key/value
 	Sequence   uint64    // Sequence identifier of document
 	VbNo       uint16    // Vbucket of document
+	DataType   uint8     // Datatype of document
 }
 
 // A Tap feed. Events from the bucket can be read from the channel returned by Events().
@@ -36,10 +37,10 @@ type TapFeed interface {
 
 // Parameters for requesting a TAP feed. Call DefaultTapArguments to get a default one.
 type TapArguments struct {
-	Backfill uint64 // Timestamp of oldest item to send. Use TapNoBackfill to suppress all past items.
-	Dump     bool   // If set, server will disconnect after sending existing items.
-	KeysOnly bool   // If true, client doesn't want values so server shouldn't send them.
-	Notify 	 BucketNotifyFn //Callback function to send notifications about lost Tap Feed
+	Backfill uint64         // Timestamp of oldest item to send. Use TapNoBackfill to suppress all past items.
+	Dump     bool           // If set, server will disconnect after sending existing items.
+	KeysOnly bool           // If true, client doesn't want values so server shouldn't send them.
+	Notify   BucketNotifyFn //Callback function to send notifications about lost Tap Feed
 }
 
 // Value for TapArguments.Backfill denoting that no past events at all should be sent.
