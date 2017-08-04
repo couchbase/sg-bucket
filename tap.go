109e536952bdc785f60a8023c6d01058b876a146
Adam Fraser <adamf@couchbase.com>
2017-08-04 15:08:44 -0700

    Support for SG #2484, and refactor naming (#26)
    * Working commit
    
    * Support for SG #2484, and refactor naming
    
    Adds distinct StartDCPFeed to bucket interface to better differentiate between TAP and DCP handling.
    
    Remove TAP from names of structs/consts used by both TAP and DCP.
    
    * Terminator support for DCP feed

--- tap.go
+++ tap.go
@@ -5,44 +5,50 @@ import (
 )
 
 // Tap operation type (found in TapEvent)
-type TapOpcode uint8
+type FeedOpcode uint8
 
 const (
-	TapBeginBackfill = TapOpcode(iota)
-	TapEndBackfill
-	TapMutation
-	TapDeletion
-	TapCheckpointStart
-	TapCheckpointEnd
+	FeedOpBeginBackfill = FeedOpcode(iota)
+	FeedOpEndBackfill
+	FeedOpMutation
+	FeedOpDeletion
+	FeedOpCheckpointStart
+	FeedOpCheckpointEnd
 )
 
 // A TAP notification of an operation on the server.
-type TapEvent struct {
-	Opcode     TapOpcode // Type of event
-	Flags      uint32    // Item flags
-	Expiry     uint32    // Item expiration time
-	Key, Value []byte    // Item key/value
-	Sequence   uint64    // Sequence identifier of document
-	VbNo       uint16    // Vbucket of document
-	DataType   uint8     // Datatype of document
-	Cas        uint64    // Cas of document
+type FeedEvent struct {
+	Opcode      FeedOpcode // Type of event
+	Flags       uint32     // Item flags
+	Expiry      uint32     // Item expiration time
+	Key, Value  []byte     // Item key/value
+	Sequence    uint64     // Sequence identifier of document
+	VbNo        uint16     // Vbucket of document
+	DataType    uint8      // Datatype of document
+	Cas         uint64     // Cas of document
+	Synchronous bool       // When true, requires that event is processed synchronously
 }
 
 // A Tap feed. Events from the bucket can be read from the channel returned by Events().
 // Remember to call Close() on it when you're done, unless its channel has closed itself already.
-type TapFeed interface {
-	Events() <-chan TapEvent      // Read only channel to read TapEvents
-	WriteEvents() chan<- TapEvent // Write only channel to write TapEvents
-	Close() error                 // Close the tap feed
+type MutationFeed interface {
+	Events() <-chan FeedEvent      // Read only channel to read TapEvents
+	WriteEvents() chan<- FeedEvent // Write only channel to write TapEvents
+	Close() error                  // Close the tap feed
 }
 
 // Parameters for requesting a TAP feed. Call DefaultTapArguments to get a default one.
-type TapArguments struct {
-	Backfill uint64         // Timestamp of oldest item to send. Use TapNoBackfill to suppress all past items.
-	Dump     bool           // If set, server will disconnect after sending existing items.
-	KeysOnly bool           // If true, client doesn't want values so server shouldn't send them.
-	Notify   BucketNotifyFn //Callback function to send notifications about lost Tap Feed
+type FeedArguments struct {
+	Backfill   uint64         // Timestamp of oldest item to send. Use TapNoBackfill to suppress all past items.
+	Dump       bool           // If set, server will disconnect after sending existing items.
+	KeysOnly   bool           // If true, client doesn't want values so server shouldn't send them.
+	Notify     BucketNotifyFn // Callback function to send notifications about lost Tap Feed
+	Terminator chan bool      // Feed will be terminated when this channel is closed (DCP Only)
 }
 
-// Value for TapArguments.Backfill denoting that no past events at all should be sent.
-const TapNoBackfill = math.MaxUint64
+// Value for TapArguments.Backfill denoting that no past events at all should be sent.  FeedNoBackfill value
+// used as actual value for walrus, go-couchbase bucket, these event types aren't defined using usual approach
+const FeedNoBackfill = math.MaxUint64
+const FeedResume = 1
+
+type FeedEventCallbackFunc func(event FeedEvent) bool
