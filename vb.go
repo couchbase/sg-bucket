2a857aeebbde1fe836ea10b159fe2ebbffc0f07a
Tor Colvin <tor.colvin@couchbase.com>
2024-04-15 14:25:38 -0400

    Improve documentation (#118)
    I noticed some of the multi xattr documentation was incorrect, fixed up
    some documentation.
    
    ---------
    
    Co-authored-by: Adam Fraser <adam.fraser@couchbase.com>

--- vb.go
+++ vb.go
@@ -10,62 +10,6 @@ licenses/APL2.txt.
 
 package sgbucket
 
-import (
-	"fmt"
-	"sync"
-)
-
-// TODO: Only Walrus uses this. Move this source file into Walrus.
-
-// Per-vbucket sequence counter
-type VbucketSeqCounter interface {
-	Incr(vbNo uint32) (uint64, error)
-	Get(vbNo uint32) (uint64, error)
-}
-
-type mapVbucketSeqCounter struct {
-	data        map[uint32]uint64
-	mu          *sync.Mutex
-	numVbuckets int
-}
-
-func NewMapVbucketSeqCounter(numVbuckets int) *mapVbucketSeqCounter {
-	return &mapVbucketSeqCounter{
-		data:        map[uint32]uint64{},
-		mu:          &sync.Mutex{},
-		numVbuckets: numVbuckets,
-	}
-}
-
-func (msq mapVbucketSeqCounter) Incr(vbNo uint32) (uint64, error) {
-	msq.mu.Lock()
-	defer msq.mu.Unlock()
-
-	if vbNo > msq.highestExpectedVbucketNo() {
-		return 0, fmt.Errorf("vbNo %v higher than max expected: %v", vbNo, msq.highestExpectedVbucketNo())
-	}
-
-	curSeq := msq.data[vbNo]
-	newSeq := curSeq + 1
-	msq.data[vbNo] = newSeq
-	return newSeq, nil
-}
-
-func (msq mapVbucketSeqCounter) Get(vbNo uint32) (uint64, error) {
-	msq.mu.Lock()
-	defer msq.mu.Unlock()
-
-	if vbNo > msq.highestExpectedVbucketNo() {
-		return 0, fmt.Errorf("vbNo %v higher than max expected: %v", vbNo, msq.highestExpectedVbucketNo())
-	}
-
-	return msq.data[vbNo], nil
-}
-
-func (msq mapVbucketSeqCounter) highestExpectedVbucketNo() uint32 {
-	return uint32(msq.numVbuckets - 1)
-}
-
 // Returns the vbucket number for a document key, produced by hashing the key string.
 // - key: The document ID
 // - numVb: The total number of vbuckets
